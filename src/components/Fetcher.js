fedde671b730cc401110defa6095693fe794e50e
Roberto Chirila <robertochirila@192.168.1.230>
2022-11-09 13:40:52 +0000

    added function that fetches the products

--- src/components/Fetcher.js
+++ src/components/Fetcher.js
@@ -4,6 +4,8 @@ import React, { useEffect } from "react"
 const Fetcher = () => {
 
     let getToken = () => {
+        let email = encodeURIComponent('adrian+1004930927@nexudus.com')
+        let password = "iFMF5dr7Vz41"
         const options = {
             method: 'POST',
             headers: {
@@ -11,10 +13,29 @@ const Fetcher = () => {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Access-Control-Allow-Origin': '*'
             },
-            grant_type: "password&username=adrian+1004930927@nexudus.compassword=iFMF5dr7Vz41"
+            body: "grant_type=password&username=" + email + "&password=" + password
+        };
+        fetch('https://cors-anywhere.herokuapp.com/https://spaces.nexudus.com/api/token', options)
+            .then(response => response.json())
+            .then(response => {
+                if (response.access_token !== null && response.access_token !== undefined) {
+                    getProducts(response.access_token)
+                }
+            }
+            )
+            .catch(err => console.error(err));
+    }
+
+    let getProducts = (access_token) => {
+        console.log(access_token)
+        const options = {
+            method: 'GET',
+            headers: {
+                authorization: 'Bearer ' + access_token
+            }
         };
 
-        fetch('https://cors-anywhere.herokuapp.com/https://spaces.nexudus.com/api/token', options)
+        fetch('https://cors-anywhere.herokuapp.com/https://spaces.nexudus.com/api/billing/products?page=1&size=25', options)
             .then(response => response.json())
             .then(response => console.log(response))
             .catch(err => console.error(err));
